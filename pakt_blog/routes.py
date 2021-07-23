a1926c1268c4d5c2f0f90d07212c133fdb8f63bf
pakt <pakt@laptop.home>
2021-07-22 17:11:58 -0700

    yooo the thing is fixed

--- pakt_blog/routes.py
+++ pakt_blog/routes.py
@@ -1,17 +1,17 @@
-from os import name
-from flask_login.utils import login_required
 from werkzeug.security import check_password_hash, generate_password_hash
 from flask import render_template, request, flash
+from flask_login.utils import login_required
 from werkzeug.utils import redirect
 
 from pakt_blog.models import db, User, Post
 
-from pakt_blog import app
+from pakt_blog import app, login_manager
 
 from flask_login import login_user
 
 @app.route("/")
 def home():
+
     return render_template("index.html", posts=db.session.query(Post).all())
 
 @app.route("/post/<id>/")
@@ -33,13 +33,19 @@ def login():
         user = User.query.filter_by(name=username).first()
 
         if not user:  
+            flash("User does not exist")
+            return redirect("/register")
+
+        correct_password = check_password_hash(user.password, password)
+
+        if correct_password:
+            login_user(user)
             return redirect("/")
 
-        if check_password_hash(user.password, password):
-            login_user(user)
-            return redirect("/create")
-        
-        return redirect("/")
+        if not correct_password:
+            flash("Incorrect credentials")
+            return redirect("/login")
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -52,6 +58,7 @@ def register():
 
         if User.query.filter_by(name=username).first():
             flash("Username already taken.")
+            return redirect("/register")
 
         user = User(name=username, password=generate_password_hash(password))
 
@@ -60,18 +67,21 @@ def register():
         db.session.commit()
 
         login_user(user)
-        
+
         return redirect("/")
 
 @app.route("/create", methods=["GET", "POST"])
+@login_required
 def create():
     if request.method == "GET":
         return render_template("create.html")   
 
     if request.method == "POST":
 
-        if Post.query.filter_by(name=name):
+        if Post.query.filter_by(name=request.form.get("title")):
             flash("Title already taken.")
+            redirect("/create")
+
 
         db.session.add(
             Post(
@@ -83,3 +93,7 @@ def create():
         db.session.commit()
 
         return redirect("/")
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect("/")
