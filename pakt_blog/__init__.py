db0b2ecedb42beb73d4c4ffcd3ec85d9db8470da
pakt <pakt@laptop.home>
2021-07-22 17:36:46 -0700

    random secret key and track modifications

--- pakt_blog/__init__.py
+++ pakt_blog/__init__.py
@@ -2,13 +2,17 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flaskext.markdown import Markdown
 from flask_login import LoginManager
+
+import os
+
 app = Flask(__name__)
 
 Markdown(app)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-app.secret_key = "pakt"
+app.secret_key = os.urandom(24)
 
 db = SQLAlchemy(app)
 
